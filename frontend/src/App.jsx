de055c58bff8bef626258f5dcaea3c36fa4e4ead
prachitvuzel <prachit678bhujel@gmail.com>
2025-07-26 18:43:17 +0545

    a basic collaborative code editor added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,52 @@
+import { useState,useRef } from 'react'
+import './App.css'
+import Editor from "@monaco-editor/react"
+import * as Y from "yjs"
+import { WebrtcProvider } from "y-webrtc"
+import {MonacoBinding} from "y-monaco"
+// import {webRTC}
+
+function App() {
+  const editorRef = useRef(null);
+  
+  function handleEditorChange(value, event) {
+    return value
+  }
+
+  function handleEditorDidMount(editor, monaco) {
+    
+      editorRef.current = editor;
+      
+        
+      const doc = new Y.Doc()
+
+      const provider = new WebrtcProvider( 'test-room', doc,{
+          signaling: ['wss://signaling.yjs.dev/',
+    'wss://y-webrtc-eu.fly.dev/',
+    'wss://y-webrtc-us.fly.dev/'], // Your local or hosted signaling server
+    })
+      const type = doc.getText("monaco")
+      const binding = new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness)
+      
+    }
+  
+
+  //  function showValue() {
+  //   alert(editorRef.current.getValue());
+  // }
+    
+  function handleEditorChange(value, event) {
+    console.log("current model value", value)
+  }
+  return (
+    <div style = {{height:"90vh", border:"1px solid black", width:"90vw"}}>
+
+      <Editor defaultLanguage="javascript" theme="vs-dark" defaultValue="//some comment" onMount={handleEditorDidMount}  onChange={handleEditorChange} />
+      
+     </div> 
+  
+  
+   )
+}
+
+export default App
